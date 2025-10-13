6837e4b51fc3279a69e98ba5ca7a4ec4b24cc7fa
BibhasShris <143662528+BibhasShris@users.noreply.github.com>
2025-10-13 14:57:48 -0600

    test

--- Fit-Journey/src/Profile.jsx
+++ Fit-Journey/src/Profile.jsx
@@ -1,6 +1,9 @@
+import ProfilePic from "./img/profilePic.png";
+
 export default function Profile({ name, age }) {
   return (
     <div>
+      <img src={ProfilePic} alt="Profile" />
       <h2>{name}</h2>
       <h3>{age}</h3>
     </div>
