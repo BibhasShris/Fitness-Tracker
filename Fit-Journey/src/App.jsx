a5506d9652e5fecf50709e0b6e4af6068a1ff635
Alek Feehan-Schuler <alfe3528@colorado.edu>
2025-10-14 17:48:41 -0600

    Profile and Daily Goals
    profile and daily goals merged together

--- Fit-Journey/src/App.jsx
+++ Fit-Journey/src/App.jsx
@@ -1,10 +1,19 @@
+import React, { useState } from "react";
 import Profile from "./Components/Profile.jsx";
+import DailyGoals from "./Components/DailyGoals/DailyGoals.jsx";
 import "./App.css";
 
 export default function App() {
+  const [today, setToday] = useState({ steps: 0, calories: 0, heartRate: 80 });
+
+  const handleUpdate = (updated) => {
+    setToday(updated);
+    console.log("Updated goals:", updated);
+  };
   return (
     <div className="tracker-layout">
-      <Profile />
+    <Profile />
+    <DailyGoals todayEntry={today} onUpdate={handleUpdate} />
     </div>
   );
 }
