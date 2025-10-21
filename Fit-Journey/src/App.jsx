e6ad1888508ce06eb9dda5320ab7a6731f7ff439
Alek Feehan-Schuler <alfe3528@colorado.edu>
2025-10-21 16:15:22 -0600

    styling and bugs

--- Fit-Journey/src/App.jsx
+++ Fit-Journey/src/App.jsx
@@ -3,8 +3,10 @@ import Profile from "./Components/Profile/Profile.jsx";
 import DailyGoals from "./Components/DailyGoals/DailyGoals.jsx";
 import Heartrate from "./Components/Heartrate/Heartrate";
 import Rewards from "./Components/RewardsForGoals/Rewards.jsx";
-import "./App.css";
 import Schedule from "./Components/Schedule/Schedule.jsx";
+import Fit from "./img/fit.png";
+import "./App.css";
+
 
 export default function App() {
   const [today, setToday] = useState({ steps: 4200, calories: 900, heartRate: 80 });
@@ -16,6 +18,13 @@ export default function App() {
   };
 
   return (
+    <>
+      {/* App header */}
+      <header className="app-header">
+        <img src={Fit} alt="Fitness icon" className="app-header-icon" />
+        <h1>Fit Journey</h1>
+      </header>
+    {/* Main Content */}
     <div className="tracker-layout">
       {/* Left Column */}
       <div className="left-column">
@@ -29,8 +38,10 @@ export default function App() {
       <div className="right-column">
         <Profile />
         <Schedule />
+        <Rewards todayEntry={today} goals={goals} />
         {/* <Schedule /> and <Rewards /> will go here later */}
       </div>
     </div>
+    </>
   );
 }
